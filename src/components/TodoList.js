a0d74014517abf5cfd9535960d0bc8820a18e90f
DavyKK <davykk04@gmail.com>
2022-08-09 17:19:18 +0300

    React tutorial

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,15 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from 'react';
+import TodoItem from './TodoItem';
 
 class TodoList extends React.Component {
   render() {
     return (
       <ul>
-      {this.props.todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo}  handleChangeProps={this.props.handleChangeProps} />
-      ))}
-    </ul>
-    )
+        {this.props.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} />
+        ))}
+      </ul>
+    );
   }
 }
 
